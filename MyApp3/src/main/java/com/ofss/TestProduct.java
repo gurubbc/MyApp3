6044989527a3ac601da4c7bda60835bb8c859133
Guru <gurumurthy.ramamurthy@gmail.com>
2020-08-06 13:15:03 +0530

    Added SOP for integration

--- MyApp3/src/main/java/com/ofss/TestProduct.java
+++ MyApp3/src/main/java/com/ofss/TestProduct.java
@@ -5,6 +5,9 @@ public class TestProduct {
 	public static void main(String[] args) {
 		Product p=new Product("SmartPhone",45000);
 		System.out.println(p.getProductName()+" and "+p.getProductPrice());
+		System.out.println("This is built through jenkins");
+		System.out.println("This is just amazing");
+		System.out.println("Integration from github to jenkins Aug 6 - 2020");
 
 	}
 
